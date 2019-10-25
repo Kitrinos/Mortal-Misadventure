9debb19beb77f7309dfe65782e870512d11e5e13
leavertonnel <leavertonnel@urbandaleschools.com>
2019-10-25 14:19:32 -0500

    just a commit

--- Into_The_Jungle.py
+++ Into_The_Jungle.py
@@ -1,4 +1,8 @@
 print("Mortal Misadventure")
 import arcade
 
-arcade.open_window(500,500, "Mortal Misadventure")
+arcade.open_window(500, 500, "Mortal Misadventure")
+
+arcade.run()
+
+arcade.set_background_color(arcade.color.BLUE)
